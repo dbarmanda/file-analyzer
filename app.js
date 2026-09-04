13f5ff995d52bed1170a51b81224aedc8fbf0211
dbarmanda <dbarmand26@gmail.com>
2026-09-04 05:41:49 +0000

    Remove 1: only main code

--- app.js
+++ app.js
@@ -1,30 +1,3 @@
-
-
-
-//1. What is process, arg vector(CLI)?
-//const args = process.argv;
-//console.log(args);
-
-//<- Output.
-// dbarm@dave:~/practice/node-practice/file-analyzer$ node app.js ./test-files
-// File analyzer started.
-// [
-//   '/home/dbarm/.nvm/versions/node/v24.20.0/bin/node',
-//   '/home/dbarm/practice/node-practice/file-analyzer/app.js',
-//   './test-files'
-// ]
-
-
-//2. Sync fs read:
-// try {
-//     const files = fs.readdirSync(directory);
-//     console.log("Files:", files);
-// } catch (error) {
-//     console.log("Unable to read directory.");
-//     console.log(error.message);
-// }
-
-
 const fs = require("fs");
 const path = require("path");
 const { text } = require("stream/consumers");
@@ -41,9 +14,6 @@ console.log("File analyzer started.");
 console.log("Directory:", directory);
 console.log("Extension:", extension);
 
-
-//doesn't directly return the file contents.
-//returns promise:
 function readFile(filePath){
     return new Promise((resolve, reject) => {
         fs.readFile(filePath, "utf8", (error, data) => {
@@ -75,44 +45,10 @@ fs.readdir(directory, (error, files)=>{
         return;
     }
     
-
-    // //starts multiple async operations, but we currently have no wawy to know when all of them have finished
-    // //comes Promises and Promise.all().
-    // files.forEach((file)=>{
-    //     const filePath = `${directory}/${file}`;
-
-    //     fs.readFile(filePath, "utf8", (error, data)=>{
-    //         if(error){
-    //             console.log("Unable to read file.");
-    //             console.log(error.message);
-    //             return;
-    //         }
-    //         console.log(`\n--- ${file} ---`);
-    //         console.log(data);
-    //     });
-    // //this forEach means-->    
-    // //start read file 1
-    // // start read file 2
-    // // finish forEach
-    // // ...
-    // // callbacks execute when their reads finish   => thus need promises.
-    // });
-
     const promises = matchingFiles.map((file) => {
         const filePath = `${directory}/${file}`;
         return readFile(filePath);
     });
-
-    // Promise.all(promises)
-    //     .then((contents) => {
-    //         console.log("All files have been read.");
-    //         console.log(contents);
-    //     })
-    //     .catch((error) => {
-    //         console.log("Unable to read files.");
-    //         console.log(error.message);
-    //     });
-    // // adding modern async await to handle promises.
 
     async function processFiles() {
         try {
